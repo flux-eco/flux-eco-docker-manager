f4c7ca8cf9ab74005163969eb6bd01a005371e1f
fluxfw <fluxfw@fluxlabs.ch>
2022-09-08 10:17:56 +0200

    wrap with compose

--- app/src/Adapters/Outbounds.php
+++ app/src/Adapters/Outbounds.php
@@ -25,14 +25,16 @@ class Outbounds implements Core\Ports\Outbounds
         $projects = [];
         foreach ($this->configs->projects as $projectId => $projectConfiguration) {
             $project = Models\DockerProject::new($projectId);
-            foreach ($projectConfiguration[ConfigurationKeyword::SOURCE_FILES->value] as $locationId => $locationConfiguration) {
-                $project->appendSourceFileLocation($locationId,
-                    Models\SourceFilesLocation::new(
-                        $locationConfiguration[ConfigurationKeyword::SOURCE->value],
-                        $projectConfiguration[ConfigurationKeyword::APP_DIRECTORY_PATH->value] . '' . $locationConfiguration[ConfigurationKeyword::APP_TARGET_SUB_PATH->value],
-                        Models\SourceFileSourcePathType::fromPath($locationConfiguration[ConfigurationKeyword::SOURCE->value])
-                    )
-                );
+            foreach ($projectConfiguration as $composeName => $composeConfiguration) {
+                foreach ($composeConfiguration[ConfigurationKeyword::SOURCE_FILES->value] as $locationId => $locationConfiguration) {
+                    $project->appendSourceFileLocation($locationId,
+                        Models\SourceFilesLocation::new(
+                            $locationConfiguration[ConfigurationKeyword::SOURCE->value],
+                            $composeConfiguration[ConfigurationKeyword::APP_DIRECTORY_PATH->value] . '' . $locationConfiguration[ConfigurationKeyword::APP_TARGET_SUB_PATH->value],
+                            Models\SourceFileSourcePathType::fromPath($locationConfiguration[ConfigurationKeyword::SOURCE->value])
+                        )
+                    );
+                }
             }
             $projects[] = $project;
         }
